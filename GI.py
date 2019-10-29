1cc3cc9ae9f10001c151e877b3b9c459158d8547
martinkarlik <mkarli18@student.aau.dk>
2019-10-29 11:44:01 +0100

    Adding recordings

--- GI.py
+++ GI.py
@@ -1,4 +1,20 @@
 # This script is the one that will control the things that are projected on to the table
 
-if __name__ == '__main__':
-    print("Vague")
+import cv2
+
+cap = cv2.VideoCapture(0)
+for i in range(0, 16):
+    print(cap.get(i))
+
+cap.set(5, 24)
+
+
+while True:
+
+    _, frame = cap.read()
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
--- UI.py
+++ UI.py
@@ -1,4 +1,13 @@
 # This script is for creating the interface that the user will interact with on the PC
 
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class FirstKivy(App):
+    def build(self):
+        return Label(text="Hello")
+
+
 if __name__ == '__main__':
-    print("Input penis")
+    FirstKivy().run()
