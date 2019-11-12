9536d75a00a3eda344949439849057e7c9c5a2f9
VictorBuch <47211196+VictorBuch@users.noreply.github.com>
2019-11-12 09:11:38 +0100

    The UI switching should work but my kivy just fucked off

--- GUI.py
+++ GUI.py
@@ -1,16 +1,3 @@
 # This script will handle the two different GUI's that we will have, just to make it easier.
 
-import cv2
-cap = cv2.VideoCapture(0)
 
-
-while cap.isOpened():
-    _, frame = cap.read()
-
-    cv2.imshow("frame", frame)
-
-    if cv2.waitKey(20) & 0xff == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
