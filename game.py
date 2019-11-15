5c57c83385f38935508d8aeb6274c7cb42e0d95f
VictorBuch <47211196+VictorBuch@users.noreply.github.com>
2019-11-15 10:56:10 +0100

    Added ui and controll flow for UI

--- game.py
+++ game.py
@@ -2,29 +2,56 @@ import pygame
 import cv2
 import algorithms
 
+table_img = ""
+
+def display_text(player_name, score, player):
+    if player == 1:
+        score = font.render(str(player_name) + ": " + str(score), True, (242, 81, 87))
+        return score
+    if player == 2:
+        score = font.render(str(player_name) + ": " + str(score), True, (7, 129, 30))
+        return score
+
+
+def change_table_img(path):
+    global table_img
+    table_img = path
+
+
+def display_table_img():
+    global table_img
+    screen.blit(pygame.image.load(table_img), (0, 0))
+
 
 if __name__ == '__main__':
 
-    cap = cv2.VideoCapture("recordings/test2_gameplay2.mp4")
+    cap = cv2.VideoCapture("recordings/black1.avi")
     beer_template_left = cv2.imread("images/testImages/templates/beer_reg_left.jpg")
     beer_template_right = cv2.imread("images/testImages/templates/beer_reg_right.jpg")
 
     pygame.init()
 
     # Create the screen
-    screen = pygame.display.set_mode((640, 480))  # just so that the whole screen isnt covered every time its run
+    screen = pygame.display.set_mode((1920, 1280))  # just so that the whole screen isnt covered every time its run
 
     # Setup the frame
     pygame.display.set_caption("BeerPong")
     icon = pygame.image.load("images/cheers.png")
     pygame.display.set_icon(icon)
 
-    tableimg1 = pygame.image.load("images/tableImages/PlaceCups.png")
+    # change_table_img(("images/tableImages/GameStarted.png"))
     circle_white = pygame.image.load("images//tableImages/circle_white.png")
+
+    # Setup general things
+    font = pygame.font.Font('freesansbold.ttf', 30)
+
+    # player variables
+    players = ['Joe', 'Jim', 'Caren', 'Ginger']
+    playersScore = [0, 0, 0, 0]
 
     app_running = True
 
-    #cropped_dimensions = algorithms.findCrop()
+    # cropped_dimensions = algorithms.findCrop()
 
     while app_running and cap.isOpened():
         _, frame = cap.read()
@@ -47,6 +74,22 @@ if __name__ == '__main__':
                     app_running = False
 
         screen.fill(0)
+
+        if not players:
+            change_table_img("images/tableImages/PlaceCups.png")
+
+        elif beers_left != 10 and beers_right != 10:
+            change_table_img("images/tableImages/GameStarted.png")
+
+        # displays that current path to the image, change image with change_table_img()
+        display_table_img()
+
+        # Display the player score and names
+        if players:
+            screen.blit(pygame.transform.rotate(display_text(players[0], playersScore[0], 1), -90), (92, 160))
+            screen.blit(pygame.transform.rotate(display_text(players[1], playersScore[1], 2), -90), (92, 870))
+            screen.blit(pygame.transform.rotate(display_text(players[2], playersScore[2], 1), 90), (1725, 160))
+            screen.blit(pygame.transform.rotate(display_text(players[3], playersScore[3], 2), 90), (1725, 870))
 
         for beer in beers_left:
             pygame.draw.circle(screen, (255, 255, 255), (beer.center[1], beer.center[0]), 5)
